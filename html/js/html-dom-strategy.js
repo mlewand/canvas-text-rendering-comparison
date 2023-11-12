51f4839d1f444a8ba95a36ac9243355bae3a89cd
Marek Lewandowski <code@mlewandowski.com>
2023-11-12 17:29:51 +0100

    Added a custom font for DOM implementation.

---
+++ html/js/html-dom-strategy.js
@@ -0,0 +1,33 @@
+
+import { CANVAS_SIZE, FONT_NAME } from './constants.js';
+
+export default {
+	name: 'Native HTML DOM',
+	shortName: 'DOM',
+
+	async init() {
+		if ( this.element ) {
+			await this.destroy();
+		}
+
+		this.element = document.createElement( 'div' );
+		this.element.style.width = `${ CANVAS_SIZE.width }px`;
+		// this.element.style.fontFamily = `Custom${ FONT_NAME }, sans-serif`;
+
+
+		return this.element;
+	},
+
+	async destroy() {
+		this.element.remove();
+		this.element = null;
+	},
+
+	async getImageUrl() {
+		return "javascript:alert('Not implemented yet')";
+	},
+
+	drawText( text ) {
+		this.element.innerText = text;
+	}
+}
