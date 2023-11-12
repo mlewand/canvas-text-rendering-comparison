f2a48bf12ca46a8daff1c5113ed47d882c6c9be5
Marek Lewandowski <code@mlewandowski.com>
2023-11-12 11:31:37 +0100

    Increased canvas size.

--- html/js/constants.js
+++ html/js/constants.js
@@ -1,6 +1,6 @@
 
 // export const CANVAS_SIZE = { width: 1000, height: 10000 };
-export const CANVAS_SIZE = { width: 1000, height: 5000 };
+export const CANVAS_SIZE = { width: 1000, height: 10254 };
 export const FONT_NAME = 'Snippet'; // good for catching differences, letters fgt are particularly unique.
 // export const FONT_NAME = 'Roboto';
 export const DPI_RATIO = 3;
